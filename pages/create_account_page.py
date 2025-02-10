84e307caba9642a595a72c4d27cd7bc1eedb164c
Nikita Shelekhov <nikita1998aa@gmail.com>
2025-02-10 20:31:53 +0000

    ad several tests

--- pages/create_account_page.py
+++ pages/create_account_page.py
@@ -1,8 +1,10 @@
-
 from pages.base_page import BasePage
 from pages.locators import create_account_locators as loc
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 
 class CreateAccountPage(BasePage):
@@ -36,17 +38,26 @@ class CreateAccountPage(BasePage):
         email_error = self.find(loc.email_error_loc)
         password_error = self.find(loc.password_error_loc)
         confirm_error = self.find(loc.confirm_error_loc)
-        assert first_name_error.text == 'This is a required field.'
-        assert last_name_error.text == 'This is a required field.'
-        assert email_error.text == 'This is a required field.'
-        assert password_error.text == 'This is a required field.'
-        assert confirm_error.text == 'This is a required field.'
+        error_text_message = 'This is a required field.'
+        assert first_name_error.text == error_text_message, f"{first_name_error.text} IS NOT {error_text_message}"
+        assert last_name_error.text == error_text_message, f"{last_name_error.text} IS NOT {error_text_message}"
+        assert email_error.text == error_text_message, f"{email_error.text} IS NOT {error_text_message}"
+        assert password_error.text == error_text_message, f"{password_error.text} IS NOT {error_text_message}"
+        assert confirm_error.text == error_text_message, f"{confirm_error.text} IS NOT {error_text_message}"
+        logging.info(f"ALL THE REQUIRED FIELDS IS EMPTY - OK IN THIS CASE")
 
-    def check_successful_registration(self):
+
+    def check_successful_registration(self, name, last_name, email):
         successful_registration_alert = self.find(loc.successful_registration_alert_loc)
         contact_information = self.find(loc.contact_information_loc)
         assert successful_registration_alert.text == 'Thank you for registering with Main Website Store.'
-        # assert contact_information.text == 'information'
+        logging.info(f"SUCCESSFUL REGISTRATION ALERT IS OK")
+        assert contact_information.text == f'{name} {last_name}\n{email}', (
+            f'{name} {last_name}\n{email} is not {contact_information.text}'
+        )
+        logging.info(f"ALL INFORMATION IS CORRECT")
 
-    def check_registration_already_email(self):
-        pass
+    def check_registration_already_email(self, error_text):
+        error_message_email = self.find(loc.error_message_email_loc)
+        assert error_text in error_message_email.text
+        logging.info(f"ERROR TEXT IS OK")
--- tests/test_create_account_page.py
+++ tests/test_create_account_page.py
@@ -1,7 +1,19 @@
-from pages.create_account_page import CreateAccountPage
 
-def test_successful_registration(driver,account_page):
-    # account_page = CreateAccountPage(driver)
+
+def test_successful_registration(account_page):
     account_page.open_page()
-    account_page.fill_the_form('Albert', 'Fedotov', 'testffewwfqf@mail.com', 'qwer@@qQ24')
+    account_page.fill_the_form('Aqlbert', 'Fqedotov', '12milo@mail.com', '1qwer@@qQ24')
     account_page.click_on_button()
+    account_page.check_successful_registration('Aqlbert', 'Fqedotov', '12milo@mail.com')
+
+def test_registration_the_same_email(account_page):
+    account_page.open_page()
+    account_page.fill_the_form('Aqlbert', 'Fqedotov', '7712551t12es1tffewwfqf@mail.com', '1qwer@@qQ24')
+    account_page.click_on_button()
+    account_page.check_registration_already_email("There is already an account with this email address")
+
+def test_required_fields(account_page):
+    account_page.open_page()
+    account_page.fill_the_form()
+    account_page.click_on_button()
+    account_page.check_required_fields_validation()
